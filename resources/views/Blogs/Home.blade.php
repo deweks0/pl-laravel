53afc142cab89600bd9824c8d659cecd6b226680
admin <55054691+Zen-dz@users.noreply.github.com>
2020-08-03 10:44:24 +0700

    init

---
+++ resources/views/Blogs/Home.blade.php
@@ -0,0 +1,9 @@
+{{-- extends to App --}}
+@extends('Templates.App')
+{{-- set content --}}
+@section('content')
+    <h1>Welcome TO My home</h1>
+@endsection
+{{-- set title --}}
+@section('title','Home')
+    
